ef5cab754e35b02cebfc76b0326a6c564b9ced9a
XU Bin <BXU1@dsone.3ds.com>
2020-02-27 18:09:49 +0100

    commit

--- src/components/Container/List/list.js
+++ src/components/Container/List/list.js
@@ -1,10 +1,82 @@
 import React, { useState } from "react";
+import { List, Icon, Typography, Avatar } from 'antd';
+
+import LoadMore from './list.loadMore';
+
 import "./list.less";
 
+const { Title, Text, Paragraph } = Typography;
+
 export default () => {
+  const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+  ];
+
   return (
     <div className="demo-list">
-      <h1>List 列表</h1>
+      <h1 className="demo-title">List 列表</h1>
+      <div className="demo-wrapper">
+        <List
+          header={
+            <div>
+              <Icon type="user" />
+              <Text copyable>User center</Text>
+              {/* <Typography.Title level={4}>User center</Typography.Title> */}
+            </div>
+          }
+          footer={
+            <Typography.Title level={4}>Footer</Typography.Title>
+          }
+          dataSource={data}
+          renderItem={(item) => {
+            return (
+              <List.Item>
+                <Typography.Text mark>[ITEM]</Typography.Text> {item}
+              </List.Item>
+            )
+          }}
+          bordered
+          size="large"
+        >
+
+        </List>
+      </div>
+      <div className="demo-wrapper">
+        <List
+          header={
+            <div>
+              <Icon type="user" />
+              <Text copyable>User center</Text>
+              {/* <Typography.Title level={4}>User center</Typography.Title> */}
+            </div>
+          }
+          footer={
+            <Typography.Title level={4}>Footer</Typography.Title>
+          }
+          dataSource={data}
+          renderItem={(item, index) => {
+            return (
+              <List.Item>
+                <List.Item.Meta
+                  style={{ padding: "10px 0 10px 30px" }}
+                  avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                  description={item}
+                  title={`Item-${index + 1}`}
+                />
+              </List.Item>
+            )
+          }}
+          bordered
+          size="large"
+          itemLayout="horizontal"
+          style={{ width: 500 }}
+        />
+      </div>
+      <LoadMore />
     </div>
   );
 }
---
+++ src/components/Container/List/list.loadMore.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import { List, Icon, Typography, Avatar, Button, Skeleton, Row, Col } from 'antd';
+import Axios from 'axios';
+
+import "./list.less";
+
+const { Text } = Typography;
+const _COUNT_ = 3;
+const fakeUserURL = `https://randomuser.me/api/?results=${_COUNT_}&inc=name,gender,email,nat&noinfo`;
+
+export default () => {
+
+    const [initLoading, setInitLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    const getData = (cb) => {
+        Axios.get(fakeUserURL, {
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(res => {
+            if (res.status === 200) {
+                const { results } = res.data;
+                cb(results);
+            }
+        })
+    }
+
+    const getMoreData = () => {
+        setLoading(true);
+        setData(data.concat(
+            [...new Array(_COUNT_)].map(
+                e => ({ loading: true, name: {}, gender: {} })
+            )
+        ));
+        getData((results) => {
+            setTimeout(() => {
+                console.log(data);
+                console.log(data.concat([...results]));
+                setData(data.concat([...results]));
+                setLoading(false);
+            }, 1500);
+        })
+    }
+
+    useEffect(() => {
+        getData((results) => {
+            setTimeout(() => {
+                setData(data.concat([...results]));
+                setLoading(false);
+                setInitLoading(false);
+            }, 1500);
+        })
+    }, []);
+
+    return (
+        <div className="demo-wrapper">
+            <List
+                header={
+                    <div>
+                        <Icon type="user" />
+                        <Text copyable>User center</Text>
+                    </div>
+                }
+                dataSource={data}
+                renderItem={(item) => {
+                    return (
+
+                        <List.Item
+                            actions={[
+                                <Icon type="edit" />,
+                                <Icon type="delete" />
+                            ]}
+                        >
+                            <Skeleton title={false} avatar loading={item.loading} active>
+                                <List.Item.Meta
+                                    avatar={
+                                        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                                    }
+                                    title={item.name.last}
+                                    description={
+                                        <div>
+                                            <Row gutter={10}>
+                                                <Col xs={24} lg={12}>Gender:</Col>
+                                                <Col xs={24} lg={12}>{item.gender}</Col>
+                                            </Row>
+                                        </div>
+                                    }
+                                />
+                            </Skeleton>
+                        </List.Item>
+
+                    )
+                }}
+                bordered
+                size="large"
+                itemLayout="horizontal"
+                style={{ width: 500 }}
+                loading={initLoading}
+                loadMore={loading || initLoading ? null :
+                    <div className="loadmore-container">
+                        <Button onClick={getMoreData}>加载更多</Button>
+                    </div>}
+            >
+            </List>
+        </div>
+    );
+}
